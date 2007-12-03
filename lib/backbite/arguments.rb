65568d3d71b7fec47084898ebb97656a14db98a5
Michael Trommer <mictro@gmail.com>
2007-12-03 23:35:25 +0100

    OptionParser added

---
+++ lib/backbite/arguments.rb
@@ -0,0 +1,76 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+module Backbite
+
+  class Options < Hash
+  end
+  
+  def self.optionparser(tlog)
+    tlog.options      ||= Options.new
+    tlog.optionparser ||= OptionParser.new(tlog)
+    yield tlog.optionparser if block_given?
+  end
+
+  def self.run_options(tlog, which, *args)
+    ret, target, skip = [], tlog.optionparser[which], 0
+
+    return nil unless target
+    
+    nargs = args.map{ |a| a.to_sym }
+    nargs.each_with_index  do |a, i|
+      unless skip.zero?
+        skip -= 1
+        next
+      end
+      if t = target[a] and arity = t.arity.abs-1
+        skip, ni = arity+1, i.succ
+        pargs =args[ni..(ni+arity)]
+        ret << t.call(*pargs)
+      else
+        Info << "Arguments: not known #{a}"
+      end
+    end
+    ret.flatten
+  end
+
+  class OptionParser
+
+    def [](obj)
+      @responder[obj.to_sym]
+    end
+    
+    def each(&blk)
+      @responder.each(&blk)
+    end
+    
+    def keyword(kw, handler)
+      @current_keyword = kw
+      @definition = @responder[@current_keyword]
+      yield(self, handler)
+    end
+    
+    def declare(keyw, &blk)
+      @responder[@current_keyword] ||= { }
+      @responder[@current_keyword][keyw] = blk
+    end
+    
+    def initialize(tlog)
+      @tlog = tlog
+      @responder = tlog.options
+    end
+  end
+  
+end
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
---
+++ spec/test_10_arguments.rb
@@ -0,0 +1,74 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+describe Backbite::OptionParser do
+
+  before(:all) do
+    @tlog = Backbite::Tumblelog.new(:rspec, 'spec/.spec_skel/default_config.rb')
+
+    Backbite.optionparser(@tlog) do |op|
+      op.keyword(:test, Object.new) do |op, handler|
+        op.declare(:single) { :single  }
+        op.declare(:one) { |a| a }
+        op.declare(:two) { |a,b| [a,b] }
+        op.declare(:three) { |a,b,c| [a,b,c] }
+      end
+      op.keyword(:bar, Object.new) do |op, handler|
+        op.declare(:single) { :single  }
+        op.declare(:one) { |a| a}
+        op.declare(:two) { |a,b| [a,b] }
+        op.declare(:three) { |a,b,c| [a,b,c] }
+      end
+    end
+    
+  end
+
+  it "should not respond to foo" do
+    Backbite.run_options(@tlog, 'test').should_not
+  end
+
+  it "should respond to a single argument" do
+    [:test, :bar].each do |fk|
+      Backbite.run_options(@tlog, fk, 'single').should == [:single]
+    end
+  end
+
+  it "should respond to a multible arguments" do
+    Backbite.run_options(@tlog, 'test', 'single', 'bar', 'single').
+      should == [:single, :single]
+  end
+
+  it "should respond to a single argument with parameters" do
+    [:test, :bar].each do |fk|
+      Backbite.run_options(@tlog, fk, 'one', 'a').should == ["a"]
+    end
+  end
+
+  it "should respond to a argument with multible parameters" do
+    [:test, :bar].each do |fk|
+      Backbite.run_options(@tlog, fk, 'two', 'a', 'b').should == ["a", "b"]
+    end
+  end
+
+  it "should respond to multible arguments with multible parameters" do
+    [[:test, :bar], [:bar, :test]].each do |fk, fkt|
+       Backbite.
+         run_options(@tlog, fk, 'two', 'a', 'b', fkt, "three", "a","b","c").
+         should == ["a", "b", "a", "b", "c"]
+    end
+  end
+
+  
+end
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
