f52b31476697a1475b307b1155c17a5549c6452d
Michael Trommer <mictro@gmail.com>
2007-12-09 03:16:09 +0100

    independent plugin: toc

---
+++ skel/plugins/toc.rb
@@ -0,0 +1,25 @@
+#
+#
+# Author:  Michael 'entropie' Trommer <mictro@gmail.com>
+#
+
+class Toc < Plugin
+
+  #ID = :toc  # optional
+  
+  def content
+    "hallo!"
+  end
+
+end
+
+
+
+=begin
+Local Variables:
+  mode:ruby
+  fill-column:70
+  indent-tabs-mode:nil
+  ruby-indent-level:2
+End:
+=end
