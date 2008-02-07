e6dbecce8e29d5a778b4a3230197f8e12d4be6f9
Michael Trommer <mictro@gmail.com>
2008-02-07 01:39:52 +0100

    mail helper

--- lib/backbite/helper/mail.rb
+++ lib/backbite/helper/mail.rb
@@ -3,16 +3,29 @@
 # Author:  Michael 'entropie' Trommer <mictro@gmail.com>
 #
 
+require 'net/smtp'
+
 module Backbite
   module Helper
 
     module Mail
+
+      def mailto(subject, msg, to, from = 'backbite')
+        msgstr = %Q(From: #{from}\nTo: #{to}\nSubject: #{subject}\n\n)
+        Net::SMTP.start('localhost') do |smtp|
+          smtp.send_message(msgstr + msg, to, from)
+        end
+      rescue
+        warn $!
+      end
+      module_function :mailto
+      
     end
 
   end
 end
 
-
+p Backbite::Helper::Mail.mailto('foo', 'bar', 'mit@particle', 'backbite')
 
 
 =begin
